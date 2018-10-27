b217d73cd89b5a402f658fbdc866404f15ff2910
Isabella DiMambro <belladimambro@gmail.com>
2018-10-27 21:08:35 +0000

    validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,9 +14,13 @@ def new
 end
 
 def create
-    current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
 def show
   @place = Place.find(params[:id])
@@ -37,15 +41,18 @@ def update
   end
 
   @place.update_attributes(place_params)
-  redirect_to root_path
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
 
 def destroy
   @place = Place.find(params[:id])
   if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
-  end 
-
+  end
 
   @place.destroy
   redirect_to root_path
